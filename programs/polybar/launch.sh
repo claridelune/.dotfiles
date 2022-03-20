2b95e8d1b909e6d17c7b35ca2b702da5e2e9e02a
CamiaCL <camilaclary@gmail.com>
2022-03-20 17:27:15 -0500

    another-bar(?

--- programs/polybar/launch.sh
+++ programs/polybar/launch.sh
@@ -5,4 +5,5 @@ killall -q polybar
 # polybar top 2>&1 | tee -a /tmp/polybar.log & disown
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-polybar -q main -c $HOME/.config/polybar/config.ini &
+polybar -c $HOME/.config/polybar/linearBar/config.ini main &
+# polybar -q main -c $HOME/.config/polybar/linearBar/config.ini &
---
+++ programs/polybar/linearBar/scripts/running_program.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+WINDOW=`bspc query -N focused -n`
+if [ ! "$WINDOW" == "" ]; then
+    NAME=`xprop -id "$WINDOW" WM_CLASS | sed 's/.*=//g' | sed 's/,.*//g' | xargs 2>/dev/null`
+else
+    NAME="desktop"
+fi
+
+[ "$NAME" == "music_player" ] && echo "%{T7}阮 %{T-} Music" && exit 0
+[ "$NAME" == "code" ] && echo "%{T7}﬏ %{T-} VS Code" && exit 0
+[ "$NAME" == "vivaldi-stable" ] && echo "%{T7} %{T-} Vivaldi" && exit 0
+[ "$NAME" == "discord" ] && echo "%{T7}碌 %{T-} Discord" && exit 0
+[ "$NAME" == "desktop" ] && echo "%{T7} %{T-} Desktop" && exit 0
+[ "$NAME" == "Alacritty" ] && echo "%{T7} %{T-} Alacritty" && exit 0
+echo "%{T7} %{T-}$NAME"
