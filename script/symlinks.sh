53a0a38968f9de21d0b4944be6e02475c4001d47
CamiaCL <camilaclary@gmail.com>
2022-03-21 11:56:45 -0500

    update symlinks

--- script/symlinks.sh
+++ script/symlinks.sh
@@ -1,9 +1,15 @@
 # Terminal emulator
 ln -sf ~/.dotfiles/programs/alacritty/ ~/.config
+# Windows Manager
 ln -sf ~/.dotfiles/programs/bspwm/ ~/.config
+# Shortcuts
 ln -sf ~/.dotfiles/programs/sxhkd ~/.config
+# Bar
 ln -sf ~/.dotfiles/programs/polybar ~/.config
+
 ln -sf ~/.dotfiles/programs/rofi ~/.config
+# Notifications
+ln -sf ~/.dotfiles/programs/dunst ~/.config
 
 # ZSH
 ln -sfn ~/.dotfiles/zsh/.zsh-plugins ~/.zsh-plugins
