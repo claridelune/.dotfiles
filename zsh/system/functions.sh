62d3262fcc972a4f2507e1c913425bf189a5445e
CamiaCL <camilaclary@gmail.com>
2022-03-23 07:12:29 -0500

    :sparkles: New functions

--- zsh/system/functions.sh
+++ zsh/system/functions.sh
@@ -1,7 +1,23 @@
-unzip(){
+unzip() {
   bsdtar xvf $1
 }
 
 fiu() {
 	fzf --preview 'bat --color=always --style=numbers --line-range=:500 {}'
+}
+
+wii() {
+  cd $HOME/Projects && lsd
+}
+
+dw() {
+  cd $HOME/Downloads && lsd
+}
+
+cf() {
+  cd $HOME/.config && lsd
+}
+
+dt() {
+  cd $HOME/.dotfiles && lsd
 }
